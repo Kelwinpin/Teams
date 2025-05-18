52570e48f69fbf5dba53267c4939b83a3d30bdf6
Kelwinpin <kelwincomw@gmail.com>
2025-05-18 10:02:26 -0300

    add statusBar

--- App.jsx
+++ App.jsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from 'styled-components';
 import theme from '@theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Loading } from '@components/Loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   
@@ -10,6 +11,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+        <StatusBar 
+          barStyle='light-content' 
+          backgroundColor='transparent'
+          translucent
+        />
         {
           fontLoaded ?
           <Groups /> 
